925fb1f115b88645ba6df0e0dfae0fb6e9e435e5
dpetrovs <dpetrovs@evolutiongaming.com>
2020-06-14 17:28:42 +0300

    Adjust script more

--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
-for repo in $(curl -u $GH_USERNAME:$GH_TOKEN -H 'Accept: application/vnd.github.v3+json' 'https://api.github.com/user/repos?type=owner&page=1' | jq '.[] | select(.fork == false) | select(.default_branch == "master") | .full_name' | sed 's/"//g')
+for repo in $(curl -u $GH_USERNAME:$GH_TOKEN -H 'Accept: application/vnd.github.v3+json' 'https://api.github.com/user/repos?type=owner&page=1' | jq '.[] | select(.fork == false) | select(.default_branch == "main") | .full_name' | sed 's/"//g')
 do
     git clone https://$GH_USERNAME:$GH_TOKEN@github.com/$repo repo_to_be_renamed
     cd repo_to_be_renamed
